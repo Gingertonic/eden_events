fe6aae64cc9c58933342a14f008c642b47eb9a8b
Beth Schofield <bethmschofield@gmail.com>
2019-06-25 20:46:41 +0000

    add validation check

--- lib/eden_events/cli.rb
+++ lib/eden_events/cli.rb
@@ -1,6 +1,9 @@
 class EdenEvents::CLI 
   def call 
-    puts 'Welcome to Eden!'
+    puts "\nWelcome to Eden!\n"
+    get_months
+    list_months
+    get_user_month
   end
   
   def get_months
@@ -8,8 +11,19 @@ class EdenEvents::CLI
   end
   
   def list_months
+    puts 'Choose a month to see events.'
     @months.each.with_index(1) do |month, index| 
-      puts "#{index}. #{month}" 
+      puts "#{index}. #{month}"
     end
   end
+  
+  def get_user_month
+    chosen_month = gets.strip
+    # if valid_input(chosen_month, @months)
+    # end
+  end 
+  
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0
+  end 
 end
