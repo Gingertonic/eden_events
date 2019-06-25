f96d1ab5e6fded08ba8da42f7a836784459d7a9c
Beth Schofield <bethmschofield@gmail.com>
2019-06-25 20:24:04 +0000

    start cli stub

--- lib/eden_events/cli.rb
+++ lib/eden_events/cli.rb
@@ -1,6 +1,15 @@
 class EdenEvents::CLI 
   def call 
     puts 'Welcome to Eden!'
-    binding.pry
+  end
+  
+  def get_months
+    @months = ['J', 'A', 'S', 'O']
+  end
+  
+  def list_months
+    @months.each.with_index(1) do |month, index| 
+      puts "#{index}. #{month}" 
+    end
   end
 end
